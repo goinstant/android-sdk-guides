5fe46cabbbc6a9afb279729ecaa1b69c5e664ea8
leadhead9 <ewisnor@gmail.com>
2015-01-29 14:57:14 -0400

    Adding profile images and updating layout

--- app/src/main/java/com/salesforce/androidsosguides/ProfileActivity.java
+++ app/src/main/java/com/salesforce/androidsosguides/ProfileActivity.java
@@ -1,6 +1,13 @@
 package com.salesforce.androidsosguides;
 
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
@@ -21,6 +28,58 @@ public class ProfileActivity extends SubActivity {
     name.setText(contact.name);
     TextView company = (TextView) findViewById(R.id.company_name);
     company.setText(contact.company);
+
+    GridView gridViewA = (GridView) findViewById(R.id.thumbnail_grid_a);
+    gridViewA.setAdapter(new ProfileThumbnailAdapter(new int[]{
+        R.drawable.profile_thumbnail_001,
+        R.drawable.profile_thumbnail_002,
+        R.drawable.profile_thumbnail_003,
+        R.drawable.profile_thumbnail_004,
+        R.drawable.profile_thumbnail_005,
+        R.drawable.profile_thumbnail_006
+    }));
+    gridViewA.getViewTreeObserver().addOnGlobalLayoutListener(new LayoutListener(gridViewA));
+
+    GridView gridViewB = (GridView) findViewById(R.id.thumbnail_grid_b);
+    gridViewB.setAdapter(new ProfileThumbnailAdapter(new int[]{
+        R.drawable.profile_thumbnail_007,
+        R.drawable.profile_thumbnail_008,
+        R.drawable.profile_thumbnail_009,
+        R.drawable.profile_thumbnail_010,
+        R.drawable.profile_thumbnail_011,
+        R.drawable.profile_thumbnail_012,
+        R.drawable.profile_thumbnail_013,
+        R.drawable.profile_thumbnail_014,
+        R.drawable.profile_thumbnail_015
+    }));
+    gridViewB.getViewTreeObserver().addOnGlobalLayoutListener(new LayoutListener(gridViewB));
+
+    GridView gridViewC = (GridView) findViewById(R.id.thumbnail_grid_c);
+    gridViewC.setAdapter(new ProfileThumbnailAdapter(new int[]{
+        R.drawable.profile_thumbnail_016,
+        R.drawable.profile_thumbnail_017,
+        R.drawable.profile_thumbnail_018
+    }));
+    gridViewC.getViewTreeObserver().addOnGlobalLayoutListener(new LayoutListener(gridViewC));
   }
 
+  private class LayoutListener implements ViewTreeObserver.OnGlobalLayoutListener {
+
+    private GridView view;
+    private boolean isResized;
+
+    public LayoutListener(GridView view) {
+      this.view = view;
+    }
+
+    @Override
+    public void onGlobalLayout() {
+      if (!isResized) {
+        ViewGroup.LayoutParams params = view.getLayoutParams();
+        params.height = (view.getHeight() * (view.getAdapter().getCount() / view.getNumColumns())) - view.getPaddingTop() - view.getPaddingBottom();
+        view.setLayoutParams(params);
+        isResized = true;
+      }
+    }
+  }
 }
---
+++ app/src/main/java/com/salesforce/androidsosguides/ProfileThumbnailAdapter.java
@@ -0,0 +1,61 @@
+package com.salesforce.androidsosguides;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+/**
+ *
+ */
+public class ProfileThumbnailAdapter extends BaseAdapter {
+
+  private int[] drawableIds;
+
+  public ProfileThumbnailAdapter(int[] drawableIds) {
+    this.drawableIds = drawableIds;
+  }
+
+  @Override
+  public int getCount() {
+    return drawableIds.length;
+  }
+
+  @Override
+  public long getItemId(int position) {
+    return drawableIds[position];
+  }
+
+  @Override
+  public Object getItem(int position) {
+    return drawableIds[position];
+  }
+
+  @Override
+  public View getView(int position, View convertView, ViewGroup parent) {
+    ImageView imageView;
+    if (convertView == null) {
+      imageView = new ImageView(parent.getContext());
+      int thumbnailSize = getColumnWidth((GridView) parent);
+      imageView.setLayoutParams(new GridView.LayoutParams(thumbnailSize, thumbnailSize));
+      imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+    } else {
+      imageView = (ImageView) convertView;
+    }
+
+    Bitmap image = BitmapFactory.decodeResource(parent.getResources(), (int) getItem(position));
+    imageView.setImageBitmap(image);
+    return imageView;
+  }
+
+  private int getColumnWidth(GridView view) {
+    if (android.os.Build.VERSION.SDK_INT >= 16) {
+      return view.getColumnWidth();
+    }
+
+    return (view.getWidth() - view.getPaddingLeft() - view.getPaddingRight()) / view.getNumColumns();
+  }
+}
