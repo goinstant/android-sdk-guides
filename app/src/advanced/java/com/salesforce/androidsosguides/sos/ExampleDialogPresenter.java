fc60c13f04b72770a04513cb9797b867a6c3de05
Matt Creaser <matt@goinstant.com>
2015-01-30 10:12:42 -0400

    (#5): Use a custom view for showing the connecting status.

--- app/src/advanced/java/com/salesforce/androidsosguides/sos/ExampleDialogPresenter.java
+++ app/src/advanced/java/com/salesforce/androidsosguides/sos/ExampleDialogPresenter.java
@@ -1,8 +1,11 @@
 package com.salesforce.androidsosguides.sos;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 
 import com.salesforce.android.sos.api.SosDefaultDialogPresenter;
+import com.salesforce.androidsosguides.MainActivity;
+import com.salesforce.androidsosguides.R;
 
 /**
  * This example dialog presenter is used to replace some of the default dialog presentation logic
@@ -11,6 +14,9 @@ import com.salesforce.android.sos.api.SosDefaultDialogPresenter;
  * not use any of the default dialog presenting logic.
  */
 public class ExampleDialogPresenter extends SosDefaultDialogPresenter {
+  /**
+   * @{inheritDoc}
+   */
   @Override
   public void show(Type type, Activity activity, Callback callback) {
 
@@ -23,7 +29,83 @@ public class ExampleDialogPresenter extends SosDefaultDialogPresenter {
       return;
     }
 
+    // Use a custom fragment to show the connection status UI on the main activity only. Other
+    // activities will use the default logic.
+    if (type == Type.CONNECTING_STATUS && activity instanceof MainActivity) {
+      showCustomFragment(activity, callback);
+      return;
+    }
+
     // Call through to the default logic for all other dialog types.
     super.show(type, activity, callback);
+  }
+
+  /**
+   * @{inheritDoc}
+   */
+  @Override
+  public void setMessage(Type type, Activity activity, int resourceId) {
+    // Update our custom status fragment on the main activity.
+    if (type == Type.CONNECTING_STATUS && activity instanceof MainActivity) {
+      setCustomFragmentMessage(activity, resourceId);
+      return;
+    }
+
+    // Call through to the default logic for all other dialog types.
+    super.setMessage(type, activity, resourceId);
+  }
+
+  /**
+   * @{inheritDoc}
+   */
+  @Override
+  public void dismiss(Type type, Activity activity) {
+    // Remove our custom status fragment on the main activity.
+    if (type == Type.CONNECTING_STATUS && activity instanceof MainActivity) {
+      dismissCustomFragment(activity);
+      return;
+    }
+
+    // Call through to the default logic for all other dialog types.
+    super.dismiss(type, activity);
+  }
+
+  // Custom fragment methods.
+
+  private void showCustomFragment(Activity activity, Callback callback) {
+    // Get the message from the type.
+    String message = Type.CONNECTING_STATUS.getMessage(activity);
+
+    // Instantiate the fragment we're going to be showing.
+    StatusFragment fragment = StatusFragment.newInstance(message, callback);
+
+    // Add the fragment to the activity.
+    activity.getFragmentManager().beginTransaction()
+        .add(R.id.status_fragment_container, fragment)
+        .commit();
+  }
+
+  private void setCustomFragmentMessage(Activity activity, int resourceId) {
+    // Find the fragment in the activity.
+    FragmentManager manager = activity.getFragmentManager();
+    StatusFragment fragment = (StatusFragment) manager.findFragmentById(R.id.status_fragment_container);
+
+    // Set the message in the fragment.
+    if (fragment != null) {
+      fragment.setMessage(activity.getResources().getString(resourceId));
+    }
+  }
+
+  private void dismissCustomFragment(Activity activity) {
+    // Find the fragment in the activity.
+    FragmentManager manager = activity.getFragmentManager();
+    StatusFragment fragment = (StatusFragment) manager.findFragmentById(R.id.status_fragment_container);
+
+    // Remove the fragment.
+    if (fragment != null) {
+      manager.beginTransaction()
+          .remove(fragment)
+          .commit();
+    }
   }
 }
