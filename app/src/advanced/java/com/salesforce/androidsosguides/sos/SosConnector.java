61f5667099b900dc792f4132dab79141f38dec97
Matt Creaser <matt@goinstant.com>
2015-03-24 13:15:18 -0300

    Bump SOS to 0.12.1

--- app/src/advanced/java/com/salesforce/androidsosguides/sos/SosConnector.java
+++ app/src/advanced/java/com/salesforce/androidsosguides/sos/SosConnector.java
@@ -3,7 +3,6 @@ package com.salesforce.androidsosguides.sos;
 import android.app.Activity;
 
 import com.salesforce.android.sos.api.Sos;
-import com.salesforce.android.sos.api.SosConfiguration;
 import com.salesforce.android.sos.api.SosDialogPresenter;
 import com.salesforce.android.sos.api.SosOptions;
 import com.salesforce.android.sos.api.SosSession;
@@ -22,14 +21,10 @@ public class SosConnector {
    * @return The new SosSession instance.
    */
   public static SosSession startSession(Activity activity, SosOptions options) {
-    // Talk to the backend that will be used when SOS becomes generally available.
-    SosConfiguration configuration =
-        new SosConfiguration.Builder().backend(SosConfiguration.Backend.GA).build();
-
     // Override the dialog presenter logic with our custom class.
     SosDialogPresenter presenter = new ExampleDialogPresenter();
 
-    return Sos.session(activity, options).configuration(configuration).dialogs(presenter).start();
+    return Sos.session(activity, options).dialogs(presenter).start();
   }
 
 }
