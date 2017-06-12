3b47343cb32c5db4e8dd76e3eed5b5c7a80be787
Steve Thistle <sthistle@salesforce.com>
2017-06-12 10:35:23 -0300

    Taking latest sos and fixing branding/overrides.

--- app/src/twoWayVideo/java/com/salesforce/androidsosguides/sos/SosConnector.java
+++ app/src/twoWayVideo/java/com/salesforce/androidsosguides/sos/SosConnector.java
@@ -1,7 +1,6 @@
 package com.salesforce.androidsosguides.sos;
 
 import android.app.Activity;
-
 import com.salesforce.android.sos.api.Sos;
 import com.salesforce.android.sos.api.SosConfiguration;
 import com.salesforce.android.sos.api.SosOptions;
@@ -15,18 +14,14 @@ public class SosConnector {
    * Start a new SOS session.
    *
    * @param activity The activity that is starting the session.
-   * @param options  The information needed to start the session.
+   * @param options The information needed to start the session.
    * @return The new SosSession instance.
    */
-  public static void startSession (Activity activity, SosOptions options) {
+  public static void startSession(Activity activity, SosOptions options) {
     // Enable the Halo UI with Two-way Video
-    SosConfiguration configuration = SosConfiguration.builder()
-        .onboardingUi(true)
-        .haloUi(true)
-        .twoWayVideo(true)
-        .build();
+    SosConfiguration configuration =
+        SosConfiguration.builder().onboardingUi(true).twoWayVideo(true).build();
 
     Sos.session(options).configuration(configuration).start(activity);
   }
-
 }
